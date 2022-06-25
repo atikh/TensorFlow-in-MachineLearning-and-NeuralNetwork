fcb0ba3c72d76517567cb48fe723ed30832a3a9a
Atieh khodadadi <46083261+atikh@users.noreply.github.com>
2022-06-25 11:37:10 +0430

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,2 +1,1 @@
 print('Learn How to use TensorFlow in MachineLearning and NeuralNetwork with details.')
-# Sources : https://www.tensorflow.org , https://colab.research.google.com/dri , Many Thanks to freeCodeCamp.org
